8fa659da1be3bfd14bd5fa550f0638e174f9b634
jaat-prince <princeteratwal17@gmail.com>
2025-02-05 00:44:45 -0800

    18 complete

--- app/components/Folder/Eighteen-page/Downpr.jsx
+++ app/components/Folder/Eighteen-page/Downpr.jsx
@@ -1,43 +1,68 @@
-import Image from 'next/image'
-import React from 'react'
-import Map from './Map'
+import Image from "next/image";
+import React from "react";
+import Map from "./Map";
 
 function Downpr() {
   return (
-         <div className='bg-[var(--Grey-2,rgba(215,217,221,0.50))] py-[60px] md:py-[80px] relative'>
-                                <div className='max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px]'>
-                               <div className='w-full absolute bottom-0 left-[0px]'>
-                                                               <Image
-                                                                                   src={"/assets/png/wave-5.png"}
-                                                                                   width={1440}
-                                                                                   height={455}
-                                                                                   alt="logo"
-                                                                                   className="w-full"
-                                                                                 />
-                                                         </div>
-                               <div className='w-full max-w-[1440px] flex flex-wrap-reverse lg:flex-nowrap'>
-                               <div className='w-full max-lg:mt-[60px] max-sm:max-w-[250px] lg:max-w-[423px]'>
-                <h1 className='text-2xl font-normal text-[var(--Brown,#494336)] max-md:ff-av'>Downtown</h1>
-                <p className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] mt-5'>2468 18th Street, SW </p>
-                <p className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)]'>Washington DC, 20003</p>
-                <h1 className='text-2xl font-normal text-[var(--Brown,#494336)] max-md:ff-av mt-6'>Opening Hours</h1>
-                <p className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] mt-5'>Monday - Friday: 8AM - 6PM</p>
-                <p className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)]'>Saturday: 9AM - 5PM</p>
-                <p className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] '>Sunday: Closed</p>
-                <h1 className='text-2xl font-normal text-[var(--Brown,#494336)] max-md:ff-av mt-6'>Opening Hours</h1>
-                <a href='#' className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] mt-5'>Phone: (212) 123-4567</a>
-                <p></p>
-                <a href='#' className='text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)]'>hello@nebulavets.com</a>
-       </div>
-                                
-                                <div className="w-full"> 
-                                           <Map/>
-                                             </div>
-                                           
-                               </div>
-                               </div>
-                                </div>
-  )
+    <div className="bg-[var(--Grey-2,rgba(215,217,221,0.50))] py-[60px] md:py-[80px] relative overflow-hidden">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12">
+        <div className="w-full absolute bottom-0 left-[0px]">
+          <Image
+            src={"/assets/png/wave-5.png"}
+            width={1440}
+            height={455}
+            alt="logo"
+            className="w-full"
+          />
+        </div>
+        <div className="w-full relative z-10 flex flex-wrap-reverse lg:flex-nowrap">
+          <div className="w-full max-lg:mt-[60px] max-sm:max-w-[250px] lg:max-w-[423px]">
+            <h1 className="text-2xl font-normal text-[var(--Brown,#494336)] max-md:ff-av">
+              Downtown
+            </h1>
+            <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] mt-5">
+              2468 18th Street, SW 
+            </p>
+            <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)]">
+              Washington DC, 20003
+            </p>
+            <h1 className="text-2xl font-normal text-[var(--Brown,#494336)] max-md:ff-av mt-6">
+              Opening Hours
+            </h1>
+            <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] mt-5">
+              Monday - Friday: 8AM - 6PM
+            </p>
+            <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)]">
+              Saturday: 9AM - 5PM
+            </p>
+            <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] ">
+              Sunday: Closed
+            </p>
+            <h1 className="text-2xl font-normal text-[var(--Brown,#494336)] max-md:ff-av mt-6">
+              Opening Hours
+            </h1>
+            <a
+              href="#"
+              className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)] mt-5"
+            >
+              Phone: (212) 123-4567
+            </a>
+            <p></p>
+            <a
+              href="#"
+              className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-[var(--Brown,#494336)]"
+            >
+              hello@nebulavets.com
+            </a>
+          </div>
+
+          <div className="w-full">
+            <Map />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Downpr
+export default Downpr;
--- app/components/Folder/Eighteen-page/Visitpr.jsx
+++ app/components/Folder/Eighteen-page/Visitpr.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Button from '../../Common/Button'
-import Image from 'next/image'
+import React from "react";
+import Button from "../../Common/Button";
+import Image from "next/image";
 
 function Visitpr() {
   return (
     <div className="bg-[#494336]">
-      <div className="max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 pt-[60px] lg:pt-0">
-        <div className="flex items-center lg:gap-[71px] flex-wrap lg:flex-nowrap md:pl-[65px]">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 pt-[60px] lg:pt-0">
+        <div className="flex items-center lg:gap-[71px] flex-wrap lg:flex-nowrap">
           <div className="w-full max-w-[568px]">
             <h1 className="text-2xl sm:text-[32px] md:text-[40px] lg:text-[48px] font-normal text-[var(--Grey,#D7D9DD)]">
-            Come visit us!
+              Come visit us!
             </h1>
-           
+
             <Button
               btn={"LEARN MORE"}
               btnclass={
@@ -19,7 +19,7 @@ function Visitpr() {
               }
             />
           </div>
-          <div className="w-full max-w-[584px] mt-7 lg:mt-0">
+          <div className="w-full max-w-[584px] mt-7 lg:mt-0 xl:mr-[-90px]">
             <Image
               src={"/assets/png/trans.png"}
               width={584}
@@ -31,7 +31,7 @@ function Visitpr() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Visitpr
+export default Visitpr;
