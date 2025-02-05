ad2b56057d069d88620ce17a420c97c2eaf7a574
jaat-prince <princeteratwal17@gmail.com>
2025-02-04 22:47:32 -0800

    12 complete

--- app/components/Folder/Telweth-page/Herotr.jsx
+++ app/components/Folder/Telweth-page/Herotr.jsx
@@ -1,32 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
-import Button from '../../Common/Button'
+import Image from "next/image";
+import React from "react";
+import Button from "../../Common/Button";
 
 function Herotr() {
   return (
-    <div className='bg-[var(--Brown,#494336)] pt-[176px] lg:pt-[140px] pb-[60px] lg:pb-[85px]'>
-    <div className='max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px] relative'>
-        <div className='absolute right-0 top-[-140px] lg:top-[-40px] w-full max-w-[225px] lg:max-w-[531px]'>
-             <Image
-                          src={"/assets/png/Travel.png"}
-                          width={531}
-                          height={421}
-                          alt="logo"
-                          className="w-full max-w-[531px]"
-                        />
+    <div className="bg-[var(--Brown,#494336)] pt-[176px] lg:pt-[140px] pb-[60px] lg:pb-[85px] relative overflow-hidden">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12">
+        <div className="absolute right-0 top-[20px] lg:top-[70px] w-full max-w-[225px] lg:max-w-[531px]">
+          <Image
+            src={"/assets/png/Travel.png"}
+            width={531}
+            height={421}
+            alt="logo"
+            className="w-full"
+          />
         </div>
-        <div>
-            <h1 className='text-[32px] sm:text-[42px] md:text-[52px] lg:text-[70px] font-normal text-[var(--Grey,#D7D9DD)] w-full max-w-[216px] sm:max-w-[614px] ff-av'>Travel</h1>
-            <p className='text-base sm:text-xl md:text-2xl font-normal text-[var(--Grey,#D7D9DD)] mt-[30px] sm:mt-5 ff-av w-full lg:max-w-[664px]'>At Nebula Vets, we understand that travel with your pet requires careful planning and adherence to specific requirements. Whether you are traveling domestically or internationally, we are here to assist you with the necessary exams and paperwork.</p>
-            <Button
-                btn={"BOOK NOW"}
-                btnclass={"mt-[45px] sm:mt-[40px] max-sm:py-[6px] max-sm:px-[19px]"}
-              />
+        <div className="relative z-10">
+          <h1 className="text-[32px] sm:text-[42px] md:text-[52px] lg:text-[70px] font-normal text-[var(--Grey,#D7D9DD)] w-full max-w-[216px] sm:max-w-[614px] ff-av">
+            Travel
+          </h1>
+          <p className="text-base sm:text-xl md:text-2xl font-normal text-[var(--Grey,#D7D9DD)] mt-[30px] sm:mt-5 ff-av w-full lg:max-w-[664px]">
+            At Nebula Vets, we understand that travel with your pet requires
+            careful planning and adherence to specific requirements. Whether you
+            are traveling domestically or internationally, we are here to assist
+            you with the necessary exams and paperwork.
+          </p>
+          <Button
+            btn={"BOOK NOW"}
+            btnclass={"mt-[45px] sm:mt-[40px] max-sm:py-[6px] max-sm:px-[19px]"}
+          />
         </div>
-    
+      </div>
     </div>
-        </div>
-  )
+  );
 }
 
-export default Herotr
+export default Herotr;
