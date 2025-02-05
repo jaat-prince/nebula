5e538dc35ea8438336bc45cbd8a51f9e6e3a2e51
jaat-prince <princeteratwal17@gmail.com>
2025-02-04 23:06:06 -0800

    14 complete

--- app/components/Folder/Fourteen-page/Heromi.jsx
+++ app/components/Folder/Fourteen-page/Heromi.jsx
@@ -1,32 +1,40 @@
-import Image from 'next/image'
-import React from 'react'
-import Button from '../../Common/Button'
+import Image from "next/image";
+import React from "react";
+import Button from "../../Common/Button";
 
 function Heromi() {
   return (
-    <div className='bg-[var(--Brown,#494336)] pt-[176px] lg:pt-[140px] pb-[60px] lg:pb-[85px]'>
-    <div className='max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px] relative'>
-        <div className='absolute right-0 top-[-140px] lg:top-[-40px] w-full max-w-[225px] lg:max-w-[624px]'>
-             <Image
-                          src={"/assets/png/Microchip.png"}
-                          width={624}
-                          height={349}
-                          alt="logo"
-                          className="w-full max-w-[624px]"
-                        />
+    <div className="bg-[var(--Brown,#494336)] pt-[176px] lg:pt-[140px] pb-[60px] lg:pb-[85px] relative overflow-hidden">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12">
+        <div className="absolute right-0 top-[37px] lg:top-[119px] w-full max-w-[309px] lg:max-w-[624px]">
+          <Image
+            src={"/assets/png/Microchip.png"}
+            width={624}
+            height={349}
+            alt="logo"
+            className="w-full"
+          />
         </div>
-        <div>
-            <h1 className='text-[32px] sm:text-[42px] md:text-[52px] lg:text-[70px] font-normal text-[var(--Grey,#D7D9DD)] w-full max-w-[216px] sm:max-w-[614px] ff-av'>Microchipping</h1>
-            <p className='text-base sm:text-xl md:text-2xl font-normal text-[var(--Grey,#D7D9DD)] mt-[30px] sm:mt-5 ff-av w-full lg:max-w-[664px]'>A microchip, resembling a tiny grain of rice in size and shape, is delicately inserted beneath your pet's skin, specifically between the shoulder blades. This quick and completely safe procedure typically takes only a few minutes. Each microchip possesses a distinct identification code, crucial for storing essential information about your beloved pet.</p>
-            <Button
-                btn={"BOOK NOW"}
-                btnclass={"mt-[45px] sm:mt-[40px] max-sm:py-[6px] max-sm:px-[19px]"}
-              />
+        <div className="relative z-10">
+          <h1 className="text-[32px] sm:text-[42px] md:text-[52px] lg:text-[70px] font-normal text-[var(--Grey,#D7D9DD)] w-full max-w-[216px] sm:max-w-[614px] ff-av">
+            Microchipping
+          </h1>
+          <p className="text-base sm:text-xl md:text-2xl font-normal text-[var(--Grey,#D7D9DD)] mt-[30px] sm:mt-5 ff-av w-full lg:max-w-[664px]">
+            A microchip, resembling a tiny grain of rice in size and shape, is
+            delicately inserted beneath your pet's skin, specifically between
+            the shoulder blades. This quick and completely safe procedure
+            typically takes only a few minutes. Each microchip possesses a
+            distinct identification code, crucial for storing essential
+            information about your beloved pet.
+          </p>
+          <Button
+            btn={"BOOK NOW"}
+            btnclass={"mt-[45px] sm:mt-[40px] max-sm:py-[6px] max-sm:px-[19px]"}
+          />
         </div>
-    
+      </div>
     </div>
-        </div>
-  )
+  );
 }
 
-export default Heromi
+export default Heromi;
--- app/components/Folder/Fourteen-page/Wisemi.jsx
+++ app/components/Folder/Fourteen-page/Wisemi.jsx
@@ -1,37 +1,82 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Wisemi() {
   return (
-    <div className='bg-[var(--Grey-2,rgba(215,217,221,0.50))] pt-[60px] md:pt-[80px] pb-[120px] md:pb-[146px] relative'>
-                 <div className='max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px]'>
-                <div className='w-full absolute bottom-0 left-[0px]'>
-                                                <Image
-                                                                    src={"/assets/png/wave-5.png"}
-                                                                    width={1440}
-                                                                    height={455}
-                                                                    alt="logo"
-                                                                    className="w-full"
-                                                                  />
-                                          </div>
-                <div className='w-full max-w-[1075px]'>
-                 <div> <h1 className='text-base sm:text-lg md:text-xl lg:text-[28px] font-normal text-[var(--Brown,#494336)]'>Here are a few compelling reasons why microchipping is a wise choice:</h1>
-                              <h1 className='text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-[45px] md:mt-[60px]'>Identification and Reunification</h1>
-                              <p className='text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]'>Pets can sometimes wander or accidentally get lost, causing distress to both the pet and their owners. A microchip provides a permanent form of identification that cannot be lost or removed. If your pet goes missing and is found by a shelter or veterinary clinic, they can scan the microchip to retrieve your contact information and swiftly reunite you with your beloved pet.</p>
-                              <h1 className='text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]'>Increased Chance of Recovery</h1>
-                              <p className='text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]'>Studies have shown that pets with microchips have a significantly higher chance of being reunited with their owners compared to those without. By having your pet microchipped, you enhance the chances of a successful reunion, saving precious time, and reducing the emotional strain of a lost pet situation.</p>
-                              <h1 className='text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]'>Secure and Permanent</h1>
-                              <p className='text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]'>Unlike collars or tags, which can break or be removed, a microchip is a tiny, implantable device that is safely and painlessly inserted under your pet's skin. Once in place, it remains with your pet for life, ensuring a permanent form of identification that cannot be tampered with or lost.</p>
-                              <h1 className='text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]'>Universal Recognition</h1>
-                              <p className='text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]'>Microchips are recognized and read by most veterinary clinics, animal shelters, and animal control agencies worldwide. This means that if your pet gets lost during travel or relocation, their microchip can be scanned and their identification information accessed regardless of where they are found.</p>
-                              <h1 className='text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]'>Medical Information Access</h1>
-                              <p className='text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]'>Advanced microchips can store additional medical information about your pet, such as vaccinations, allergies, or any ongoing medical conditions. This feature can be especially helpful in emergency situations when immediate access to crucial health details can make a significant difference in the treatment and care your pet receives.</p>
-                            
-                              </div>
-                </div>
-                </div>
-                 </div>
-  )
+    <div className="bg-[var(--Grey-2,rgba(215,217,221,0.50))] pt-[60px] md:pt-[80px] pb-[120px] md:pb-[146px] relative overflow-hidden">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12">
+        <div className="w-full absolute bottom-0 left-[0px]">
+          <Image
+            src={"/assets/png/wave-5.png"}
+            width={1440}
+            height={455}
+            alt="logo"
+            className="w-full"
+          />
+        </div>
+        <div className="w-full relative z-10">
+          <div>
+            <h1 className="text-base sm:text-lg md:text-xl lg:text-[28px] font-normal text-[var(--Brown,#494336)]">
+              Here are a few compelling reasons why microchipping is a wise
+              choice:
+            </h1>
+            <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-[45px] md:mt-[60px]">
+              Identification and Reunification
+            </h1>
+            <p className="text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]">
+              Pets can sometimes wander or accidentally get lost, causing
+              distress to both the pet and their owners. A microchip provides a
+              permanent form of identification that cannot be lost or removed.
+              If your pet goes missing and is found by a shelter or veterinary
+              clinic, they can scan the microchip to retrieve your contact
+              information and swiftly reunite you with your beloved pet.
+            </p>
+            <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]">
+              Increased Chance of Recovery
+            </h1>
+            <p className="text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]">
+              Studies have shown that pets with microchips have a significantly
+              higher chance of being reunited with their owners compared to
+              those without. By having your pet microchipped, you enhance the
+              chances of a successful reunion, saving precious time, and
+              reducing the emotional strain of a lost pet situation.
+            </p>
+            <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]">
+              Secure and Permanent
+            </h1>
+            <p className="text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]">
+              Unlike collars or tags, which can break or be removed, a microchip
+              is a tiny, implantable device that is safely and painlessly
+              inserted under your pet's skin. Once in place, it remains with
+              your pet for life, ensuring a permanent form of identification
+              that cannot be tampered with or lost.
+            </p>
+            <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]">
+              Universal Recognition
+            </h1>
+            <p className="text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]">
+              Microchips are recognized and read by most veterinary clinics,
+              animal shelters, and animal control agencies worldwide. This means
+              that if your pet gets lost during travel or relocation, their
+              microchip can be scanned and their identification information
+              accessed regardless of where they are found.
+            </p>
+            <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal mt-5 lg:mt-[30px]">
+              Medical Information Access
+            </h1>
+            <p className="text-[var(--Brown,#494336)] text-sm sm:text-base md:text-lg lg:text-xl font-normal ff-av mt-5 lg:mt-[30px]">
+              Advanced microchips can store additional medical information about
+              your pet, such as vaccinations, allergies, or any ongoing medical
+              conditions. This feature can be especially helpful in emergency
+              situations when immediate access to crucial health details can
+              make a significant difference in the treatment and care your pet
+              receives.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Wisemi
+export default Wisemi;
