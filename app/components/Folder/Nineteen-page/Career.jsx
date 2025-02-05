d731d6b8cbeeaa131ed94eb0efe0882b7ce5f350
jaat-prince <princeteratwal17@gmail.com>
2025-02-05 01:01:48 -0800

    19 complete

--- app/components/Folder/Nineteen-page/Career.jsx
+++ app/components/Folder/Nineteen-page/Career.jsx
@@ -5,17 +5,17 @@ import { jionData } from "../../Helper/Helper";
 
 function Career() {
   return (
-    <div className="relative">
-      <div className="max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px] py-[60px] md:py-[80px]">
-        <div className="absolute left-0 w-full bottom-0 max-md:hidden ">
-          <Image
-            src={"/assets/png/wave-7.png"}
-            width={1440}
-            height={376}
-            alt="logo"
-            className="w-full"
-          />
-        </div>
+    <div className="relative overflow-hidden">
+      <div className="absolute left-0 w-full bottom-0 max-md:hidden ">
+        <Image
+          src={"/assets/png/wave-7.png"}
+          width={1440}
+          height={376}
+          alt="logo"
+          className="w-full"
+        />
+      </div>
+      <div className="max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px] py-[60px] md:py-[80px] relative z-10">
         <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal w-full max-sm:max-w-[274px]">
           Why Choose a Career at Nebula Vets:
         </h1>
