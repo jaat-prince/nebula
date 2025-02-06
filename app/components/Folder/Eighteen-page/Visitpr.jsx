a6b0159b7d01754d6c5baf17da07f9b87d5b74a3
jaat-prince <princeteratwal17@gmail.com>
2025-02-05 21:05:29 -0800

    change in page 18

--- app/components/Folder/Eighteen-page/Visitpr.jsx
+++ app/components/Folder/Eighteen-page/Visitpr.jsx
@@ -4,8 +4,17 @@ import Image from "next/image";
 
 function Visitpr() {
   return (
-    <div className="bg-[#494336]">
-      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 pt-[60px] lg:pt-0">
+    <div className="bg-[#494336] relative">
+      <div className="absolute bottom-0 right-0 w-full max-w-[288px] min-[500px]:max-w-[400px] md:max-w-[580px] lg::max-w-[626px] ">
+        <Image
+          src={"/assets/png/trans.png"}
+          width={584}
+          height={372}
+          alt="logo"
+          className="w-full"
+        />
+      </div>
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 pt-[60px] lg:pt-[117px] pb-[194px] lg:pb-[116px]">
         <div className="flex items-center lg:gap-[71px] flex-wrap lg:flex-nowrap">
           <div className="w-full max-w-[568px]">
             <h1 className="text-2xl sm:text-[32px] md:text-[40px] lg:text-[48px] font-normal text-[var(--Grey,#D7D9DD)]">
@@ -15,17 +24,8 @@ function Visitpr() {
             <Button
               btn={"LEARN MORE"}
               btnclass={
-                "py-[6px] px-[10px] sm:py-[12px] sm:px-[27px] mt-[38px] sm:mt-9"
+                "py-[6px] px-[10px] sm:py-[12px] sm:px-[27px] mt-[40px] sm:mt-[73px]"
               }
-            />
-          </div>
-          <div className="w-full max-w-[584px] mt-7 lg:mt-0 xl:mr-[-90px]">
-            <Image
-              src={"/assets/png/trans.png"}
-              width={584}
-              height={372}
-              alt="logo"
-              className="w-full lg:max-w-[584px]"
             />
           </div>
         </div>
