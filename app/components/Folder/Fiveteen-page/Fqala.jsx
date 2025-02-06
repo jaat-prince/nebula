0452a56ed7f1b5a3f6f2a2c6640b7c4007258b70
jaat-prince <princeteratwal17@gmail.com>
2025-02-05 20:42:06 -0800

    change in page 15

--- app/components/Folder/Fiveteen-page/Fqala.jsx
+++ app/components/Folder/Fiveteen-page/Fqala.jsx
@@ -9,7 +9,7 @@ function Fqala() {
         <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal w-full max-sm:max-w-[274px]">
           Benefits of Laser Therapy:
         </h1>
-        <div className="mt-2 w-full max-w-[1080px]">
+        <div className="mt-2 w-full">
           {laserData.map((item, index) => (
             <Ques key={index} title={item.title} text={item.text} />
           ))}
