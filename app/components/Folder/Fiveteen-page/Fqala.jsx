41669e7579ec5a44bbfe7164671cd427aa963f45
jaat-prince <princeteratwal17@gmail.com>
2025-02-05 20:41:45 -0800

    change in page 15

--- app/components/Folder/Fiveteen-page/Fqala.jsx
+++ app/components/Folder/Fiveteen-page/Fqala.jsx
@@ -5,13 +5,13 @@ import { laserData } from "../../Helper/Helper";
 function Fqala() {
   return (
     <div>
-      <div className="max-w-[1440px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 md:ml-[65px] py-[60px] md:py-[80px]">
+      <div className="max-w-[1180px] mx-auto px-4 sm:px-8 md:px-10 lg:px-12 py-[60px] md:py-[80px]">
         <h1 className="text-[var(--Brown,#494336)] ff-av text-2xl md:text-[32px] lg:text-[36px] font-normal w-full max-sm:max-w-[274px]">
           Benefits of Laser Therapy:
         </h1>
         <div className="mt-2 w-full max-w-[1080px]">
           {laserData.map((item, index) => (
-            <Ques key={index} title={item.text} text={item.text} />
+            <Ques key={index} title={item.title} text={item.text} />
           ))}
         </div>
       </div>
